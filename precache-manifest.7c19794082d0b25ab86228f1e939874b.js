731673b15e9f26cdc672869e9a19118ecd0bf47a
siva1005 <siva.krishna@impactanalytics.co>
2020-10-13 18:47:17 +0530

    Updates

---
+++ precache-manifest.7c19794082d0b25ab86228f1e939874b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78706a52074de3be44741cc359d8e219",
+    "url": "/adobe-assignment/index.html"
+  },
+  {
+    "revision": "7ffc440abdeb5c247dc9",
+    "url": "/adobe-assignment/static/css/main.6e474a81.chunk.css"
+  },
+  {
+    "revision": "d79c36266c644b5ad4c7",
+    "url": "/adobe-assignment/static/js/2.c1f3ba3a.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/adobe-assignment/static/js/2.c1f3ba3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ffc440abdeb5c247dc9",
+    "url": "/adobe-assignment/static/js/main.9ad1a402.chunk.js"
+  },
+  {
+    "revision": "8686f73f7208263b2207",
+    "url": "/adobe-assignment/static/js/runtime-main.6dd466a4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/adobe-assignment/precache-manifest.7c19794082d0b25ab86228f1e939874b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adobe-assignment/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
